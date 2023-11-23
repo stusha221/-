59579a6a268f3b2364fee0d2ddc3a79762c37a2e
stush <stush@STUSHA>
2023-11-23 20:51:54 +0900

    Добавьте файлы проекта.

---
+++ Текстовый редактор/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Текстовый_редактор
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tbVvod.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text documents (*.txt)|*.txt|Word documents (*.doc)|*.doc";
+            if (openFileDialog.ShowDialog() == true)
+            {   string filename = openFileDialog.FileName;
+                string fileText = File.ReadAllText(filename);
+                tbVvod.Text = fileText;
+                MessageBox.Show("Файл открыт!");
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|Word Documents (*.doc)|*.doc";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filename = saveFileDialog.FileName;
+                File.WriteAllText(filename, tbVvod.Text);
+                MessageBox.Show("Файл сохранён!");
+            }
+        }
+    }
+}
